65c3984b9353c2674c1b1d89c5858d6469cbe186
benibela <benito@benibela.de>
2012-01-30 19:01:44 +0100

    don't use desktop-file-install because it crashed
    --HG--
    branch : trunks

--- _meta/install.sh
+++ _meta/install.sh
@@ -4,6 +4,7 @@ mkdir -p /usr/share/videlibri/ &&
 cp videlibri /usr/bin/ &&
 cp -R data /usr/share/videlibri 
 echo "install desktop"
-desktop-file-install videlibri.desktop
+#desktop-file-install videlibri.desktop # #stupid thing crashes
+cp videlibri.desktop /usr/share/applications
 echo "autostart"
 cp videlibri.desktop ~/.config/autostart/
