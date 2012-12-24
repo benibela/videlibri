e0f162ecd7198f1ac9a12696cbbeb0d8472cbacc
benibela <benito@benibela.de>
2012-12-24 15:14:48 +0100

    localized manage script
    --HG--
    branch : trunks

---
+++ manage.sh
@@ -0,0 +1,38 @@
+#/bin/bash
+
+
+source ../../../manageUtils.sh
+
+sfProject videlibri
+
+VIDELIBRIBASE=$HGROOT/programs/internet/VideLibri
+
+function pushhg(){
+  PUBLICHG=$HGROOT/../videlibrixidelpublichg
+  syncHg $VIDELIBRIBASE/_hg.filemap $HGROOT $PUBLICHG
+}
+
+
+INTVERSION=`grep versionNumber:integer $VIDELIBRIBASE/applicationconfig.pas | grep -oE "[0-9]+" `;
+VERSION=`echo "scale=3;$INTVERSION/1000" | bc`;
+VERSION=`export LC_NUMERIC=C; printf "%1.3f\n" $VERSION`;
+
+case "$1" in
+hg)  
+  pushhg
+  cd $VIDELIBRIBASE/_meta/version/
+  webUpload version.xml changelog.xml /updates/
+;;
+
+src)
+  pushhg
+  SRCDIR=/tmp/videlibri-$VERSION-src
+  rm -R $SRCDIR
+  cp -r $PUBLICHG $SRCDIR
+  cd /tmp
+  rm -Rvf $SRCDIR/programs/internet/xidel $SRCDIR/programs/internet/sourceforgeresponder/
+  tar -cvzf /tmp/videlibri-$VERSION.src.tar.gz --exclude=.hg videlibri-$VERSION-src
+  fileUpload videlibri-$VERSION.src.tar.gz "/VideLibri\ $VERSION/"
+
+esac
+
