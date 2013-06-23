c384356cebaad3deb847c1b5545d4e505fa0e866
benibela <benito@benibela.de>
2013-06-23 14:13:42 +0200

    increase some version numbers (for android)
    --HG--
    branch : trunks

--- applicationconfig.pas
+++ applicationconfig.pas
@@ -36,7 +36,7 @@ var programPath,userPath:string;
     nextLimitStr: string;
 
     appFullTitle:string='VideLibri';
-    versionNumber:integer=1402;
+    versionNumber:integer=1500;
     //=>versionNumber/1000
     newVersionInstalled: boolean=false;
 
