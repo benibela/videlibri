ed995e657c8b89e5e7fa783094461553904c6e61
benibela <benito@benibela.de>
2014-03-24 00:33:01 +0100

    working upload script (mysql)
    --HG--
    branch : trunks

--- _meta/sfsite/user/upload.php
+++ _meta/sfsite/user/upload.php
@@ -1,27 +1,22 @@
  <?php
-    $fp = fopen("user.id", "r+");
-    if (flock($fp, LOCK_EX)) {
-        //print "Got lock!\n";
-        $i = 0;
-        fscanf($fp, "%d", $i);
-        
-        $i = $i + 1;
-        fseek($fp, 0);
+    $i = 1;
+    $link = mysql_connect('mysql-v', 'v359854rw', '9VbVsgfyrl57');
+    if (!$link) {
+        die('Verbindung zum Datenbankserver fehlgeschlagen');
+    }    
+    mysql_select_db("v359854_upload", $link);
+    mysql_query("INSERT INTO id () values ()", $link);
+    $i = mysql_insert_id($link);    
+    mysql_close($link);
+    
 
-        fprintf($fp, "%d", $i);
-        
-        flock($fp, LOCK_UN);
-        fclose($fp);
-        
-        mkdir("./".$i);
-        mkdir("./".$i."/template");
-        
-        file_put_contents ( "./".$i."/meta.xml" , $_POST["meta"]);
-        file_put_contents ( "./".$i."/bib.html" , $_POST["links"]);
-        file_put_contents ( "./".$i."/template/template" , $_POST["template"]);
-        
-        print "<span>Hochgeladen zu: <code>http://videlibri.sf.net/user/".$i."/bib.html</code></span>";
-    } else {
-       print "Automatisches Hochladen fehlgeschlagen! "; 
-    }
+    
+    mkdir("./".$i);
+    mkdir("./".$i."/template");
+    
+    file_put_contents ( "./".$i."/meta.xml" , $_POST["meta"]);
+    file_put_contents ( "./".$i."/bib.html" , $_POST["links"]);
+    file_put_contents ( "./".$i."/template/template" , $_POST["template"]);
+    
+    print "<span>Hochgeladen zu: <code>http://videlibri.sf.net/user/".$i."/bib.html</code></span>";
 ?> 
