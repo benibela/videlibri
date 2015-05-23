d7b2b406e4c08c7495757d5a10e70c0b4c853a3a
benibela <benito@benibela.de>
2015-05-23 16:16:30 +0200

    fix last commit

--- libraryaccess.pas
+++ libraryaccess.pas
@@ -404,7 +404,7 @@ begin
 
     for i:=0 to accounts.count-1 do
       if accounts[i].enabled then
-        result := result or updateAccountBookData(account, ignoreConnErrors, checkDate, extendAlways);
+        result := result or updateAccountBookData(accounts[i], ignoreConnErrors, checkDate, extendAlways);
     exit;
   end;
 
