28b02eea4ebc13aed97234cc2cebd41664274638
benibela <benito@benibela.de>
2016-02-18 00:44:59 +0100

    version stuff

--- _meta/installer/videlibri.iss
+++ _meta/installer/videlibri.iss
@@ -1,7 +1,7 @@
 [Setup]
 ;Change this in every version
-AppVerName=VideLibri 1.820
-AppVersion=1.820
+AppVerName=VideLibri 1.830
+AppVersion=1.830
 
 ;Don't change
 AppName=VideLibri
--- applicationconfig.pas
+++ applicationconfig.pas
@@ -48,7 +48,7 @@ var programPath,userPath:string;
     nextLimitStr: string;
 
     appFullTitle:string='VideLibri';
-    versionNumber:integer=1820     ;
+    versionNumber:integer=1830     ;
     //=>versionNumber/1000
     newVersionInstalled: boolean=false;
 
