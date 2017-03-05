c34b6e2c6615df3f8bc0e6629de3101441854287
benibela <benito@benibela.de>
2017-03-05 13:57:08 +0100

    ...

--- _meta/sites/unittests.sh
+++ _meta/sites/unittests.sh
@@ -223,8 +223,7 @@ PAGES=(${PAGES[@]} summon/searchDetails.html summon/searchDetails_multiex.html s
 
 #=============netbiblio==============
 mkdir -p $OUTPATH/netbiblio
-test netbiblio list list.html 
-test netbiblio listOrders list.orders.html 
+test netbiblio list list.html list.orders.html 
 
 test netbiblio searchList searchList.0.html searchList.html 
 test netbiblio searchDetails{vl:set-book-property} searchDetails.html searchDetails.ebook.html 
