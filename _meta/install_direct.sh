9a8a8076bc17dbb32b7d5ad4544e2d30ebc65d3c
benibela <benito@benibela.de>
2017-09-14 22:51:55 +0200

    fix deb script

--- _meta/install_direct.sh
+++ _meta/install_direct.sh
@@ -2,8 +2,7 @@
 builddir="$1"
 
 echo "copying files"
-mkdir -p $builddir/usr/share/videlibri/ &&
-mkdir -p $builddir/usr/share/videlibri/data &&  
+mkdir -p $builddir/usr/share/videlibri/ $builddir/usr/share/videlibri/data $builddir/usr/share/applications &&
 cp videlibri $builddir/usr/bin/ &&
 cp -R data/libraries $builddir/usr/share/videlibri/data &&
 cp data/lclstrconsts.de.po $builddir/usr/share/videlibri/data &&
@@ -12,9 +11,9 @@ cp data/videlibri.chm $builddir/usr/share/videlibri/data &&
 cp data/*.png $builddir/usr/share/videlibri/data &&
 cp data/smallYellow.ico $builddir/usr/share/videlibri/data &&
 cp _meta/installer/add/data/machine.config $builddir/usr/share/videlibri/data  
-echo "install desktop"
 
+echo "install desktop"
 #desktop-file-install videlibri.desktop #stupid thing crashes
-cp videlibri.desktop $builddir/usr/share/applications
+cp videlibri.desktop $builddir/usr/share/applications/
 
 
