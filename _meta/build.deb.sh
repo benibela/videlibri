f999188e0ff3c9b297000bdbef8b4d97a2dc2c2c
benibela <benito@benibela.de>
2017-11-16 00:01:32 +0100

    fix lintian issues

--- _meta/build.deb.sh
+++ _meta/build.deb.sh
@@ -13,14 +13,14 @@ VERSION=`export LC_NUMERIC=C; printf "%1.3f\n" $VERSION`;
 rm -rf $builddir/
 mkdir -p $builddir/DEBIAN $builddir/usr/bin $builddir/usr/share/doc/videlibri $builddir/usr/share/videlibri
 $metadir/install_direct.sh $builddir
+strip --strip-all $builddir/usr/bin/videlibri
 cp -r $debiandir/control $debiandir/conffiles $builddir/DEBIAN/
 cp $debiandir/copyright $builddir/usr/share/doc/videlibri/copyright
 xidel $metadir/version/changelog.xml -e '/changelog/build!(
   x"videlibri ({format-number(xs:decimal(@version)*0.001, "0.000")}) unstable; urgency=low", 
    "", .//(fix,add,change)/("  * "||.),
    "",
-   " -- Benito van der Zander <benito@benibela.de>  "||format-date(xs:date(@date), "[FNn,3-3], [D] [MNn,3-3] [Y]")||" 00:00:00 +0100", "" )' > $builddir/DEBIAN/changelog
-gzip -9 -n -c < $builddir/DEBIAN/changelog > $builddir/usr/share/doc/videlibri/changelog.gz
+   " -- Benito van der Zander <benito@benibela.de>  "||format-date(xs:date(@date), "[FNn,3-3], [D] [MNn,3-3] [Y]")||" 00:00:00 +0100", "" )' | gzip -9 -n -c | $builddir/usr/share/doc/videlibri/changelog.gz
 
 
 sed -Ee "s/Version:.*/Version: $VERSION/" -i $builddir/DEBIAN/control
