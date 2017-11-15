7a534bae4c5260f41ae0af8bf31732ece3fa43c2
benibela <benito@benibela.de>
2017-11-15 23:39:44 +0100

    better deb creation

--- _meta/build.deb.sh
+++ _meta/build.deb.sh
@@ -14,13 +14,13 @@ rm -rf $builddir/
 mkdir -p $builddir/DEBIAN $builddir/usr/bin $builddir/usr/share/doc/videlibri $builddir/usr/share/videlibri
 $metadir/install_direct.sh $builddir
 cp -r $debiandir/control $debiandir/conffiles $builddir/DEBIAN/
-gzip -9 -n -c $debiandir/changelog > $builddir/usr/share/doc/videlibri/changelog.gz
 cp $debiandir/copyright $builddir/usr/share/doc/videlibri/copyright
 xidel $metadir/version/changelog.xml -e '/changelog/build!(
   x"videlibri ({format-number(xs:decimal(@version)*0.001, "0.000")}) unstable; urgency=low", 
    "", .//(fix,add,change)/("  * "||.),
    "",
    " -- Benito van der Zander <benito@benibela.de>  "||format-date(xs:date(@date), "[FNn,3-3], [D] [MNn,3-3] [Y]")||" 00:00:00 +0100", "" )' > $builddir/DEBIAN/changelog
+gzip -9 -n -c < $builddir/DEBIAN/changelog > $builddir/usr/share/doc/videlibri/changelog.gz
 
 
 sed -Ee "s/Version:.*/Version: $VERSION/" -i $builddir/DEBIAN/control
