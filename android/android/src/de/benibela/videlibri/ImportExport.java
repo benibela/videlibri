48356130a6d8e228c846f2276d4064c2dee6b808
benibela <benito@benibela.de>
2018-01-04 00:47:04 +0100

    improve import/export layout, path selection button

--- android/android/src/de/benibela/videlibri/ImportExport.java
+++ android/android/src/de/benibela/videlibri/ImportExport.java
@@ -1,10 +1,17 @@
 package de.benibela.videlibri;
 
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -198,6 +205,17 @@ public class ImportExport extends VideLibriBaseActivity {
             }
         });
 
+        findButtonById(R.id.buttonChoose).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                //intent.putExtra(Intent.CAT, true);
+                intent.putExtra("android.intent.extra.LOCAL_ONLY", true);
+                startActivityForResult(intent, 30017);
+            }
+        });
     }
 
     @Override
@@ -217,5 +235,119 @@ public class ImportExport extends VideLibriBaseActivity {
                 return true;
         }
         return super.onDialogResult(dialogId, buttonId, more);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 30017) {
+            String path = UriToPath.getPath(this, data.getData());
+            if (path != null) setEditTextText(R.id.edit, path);
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    static private class UriToPath {
+        //https://stackoverflow.com/a/36129285
+        public static String getPath(Context context, Uri uri) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                if (DocumentsContract.isDocumentUri(context, uri)) {
+                    // ExternalStorageProvider
+                    if (isExternalStorageDocument(uri)) {
+                        final String docId = DocumentsContract.getDocumentId(uri);
+                        final String[] split = docId.split(":");
+                        final String type = split[0];
+
+                        if ("primary".equalsIgnoreCase(type)) {
+                            return Environment.getExternalStorageDirectory() + "/" + split[1];
+                        }
+                        return Environment.getExternalStorageDirectory() + "/" + split[1]; //??
+                    }
+                    // DownloadsProvider
+                    else if (isDownloadsDocument(uri)) {
+                        final String id = DocumentsContract.getDocumentId(uri);
+                        final Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                        return getDataColumn(context, contentUri, null, null);
+                    }
+                    // MediaProvider
+                    else if (isMediaDocument(uri)) {
+                        final String docId = DocumentsContract.getDocumentId(uri);
+                        final String[] split = docId.split(":");
+                        final String type = split[0];
+                        Uri contentUri = null;
+                        if ("image".equals(type)) {
+                            contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                        } else if ("video".equals(type)) {
+                            contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                        } else if ("audio".equals(type)) {
+                            contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                        }
+                        final String selection = "_id=?";
+                        final String[] selectionArgs = new String[]{split[1]};
+                        return getDataColumn(context, contentUri, selection, selectionArgs);
+                    }
+                }
+            }
+
+            // MediaStore (and general)
+            if ("content".equalsIgnoreCase(uri.getScheme())) {
+                // Return the remote address
+                if (isGooglePhotosUri(uri))
+                    return uri.getLastPathSegment();
+                return getDataColumn(context, uri, null, null);
+            }
+            // File
+            if ("file".equalsIgnoreCase(uri.getScheme())) {
+                return uri.getPath();
+            }
+            return null;
+        }
+
+        public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+            Cursor cursor = null;
+            final String column = "_data";
+            final String[] projection = {column};
+            try {
+                cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+                if (cursor != null && cursor.moveToFirst()) {
+                    final int index = cursor.getColumnIndexOrThrow(column);
+                    return cursor.getString(index);
+                }
+            } finally {
+                if (cursor != null)
+                    cursor.close();
+            }
+            return null;
+        }
+
+        public static boolean isExternalStorageDocument(Uri uri) {
+            return "com.android.externalstorage.documents".equals(uri.getAuthority());
+        }
+
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is DownloadsProvider.
+         */
+        public static boolean isDownloadsDocument(Uri uri) {
+            return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+        }
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is MediaProvider.
+         */
+        public static boolean isMediaDocument(Uri uri) {
+            return "com.android.providers.media.documents".equals(uri.getAuthority());
+        }
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is Google Photos.
+         */
+        public static boolean isGooglePhotosUri(Uri uri) {
+            return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+        }
     }
 }
