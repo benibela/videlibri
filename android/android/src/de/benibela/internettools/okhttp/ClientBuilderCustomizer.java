647d01f8e0110cd1ef6db8037f6be7cc07b88fca
benibela <benito@benibela.de>
2018-06-30 17:08:46 +0200

    okhttp: enable large timeouts and all connection specs

--- android/android/src/de/benibela/internettools/okhttp/ClientBuilderCustomizer.java
+++ android/android/src/de/benibela/internettools/okhttp/ClientBuilderCustomizer.java
@@ -7,11 +7,15 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.X509TrustManager;
 
 import de.benibela.internettools.ModernSSLSocketFactory;
 import de.benibela.internettools.X509TrustManagerWithAdditionalKeystores;
+import okhttp3.ConnectionSpec;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -20,6 +24,12 @@ import okhttp3.Response;
 public class ClientBuilderCustomizer {
     static void customize(OkHttpClient.Builder builder) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
         X509TrustManager tm = new X509TrustManagerWithAdditionalKeystores();
+        builder.connectTimeout(5, TimeUnit.MINUTES);
+        builder.readTimeout(5, TimeUnit.MINUTES);
+        builder.writeTimeout(5, TimeUnit.MINUTES);
+        ArrayList<ConnectionSpec> temp = new ArrayList<>();
+        temp.add(new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS).allEnabledCipherSuites().allEnabledTlsVersions().build());
+        builder.connectionSpecs(temp);
         builder.sslSocketFactory(new ModernSSLSocketFactory(tm), tm);
         /*builder.addInterceptor(new Interceptor() {
             @Override
