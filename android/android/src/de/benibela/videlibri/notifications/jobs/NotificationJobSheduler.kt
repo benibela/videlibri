6caef2c9b664391fc5e4668f4e300333505dcfc7
benibela <benito@benibela.de>
2018-11-18 19:18:14 +0100

    refactor notification job stop to separate handling of jobs with different id

--- android/android/src/de/benibela/videlibri/notifications/jobs/NotificationJobSheduler.kt
+++ android/android/src/de/benibela/videlibri/notifications/jobs/NotificationJobSheduler.kt
@@ -14,10 +14,15 @@ import de.benibela.videlibri.VideLibriApp
 import de.benibela.videlibri.notifications.NotificationScheduling
 import de.benibela.videlibri.notifications.Notifier
 
+private const val JOB_ID_DAILY = 2350
+private const val JOB_ID_AFTER_BOOT = 2351
+
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class NotificationJobService: JobService() {
+    private fun jobIndex(params: JobParameters?): Int =  (params?.jobId?:0) - JOB_ID_DAILY
+
     override fun onStartJob(params: JobParameters?): Boolean {
-        Log.d("VIDELIBRI JOB", "onStartJob!!");
+        Log.d("VIDELIBRI JOB", "onStartJob!!")
         if (!NotificationScheduling.preferenceNotificationsEnabled(this))
             return false
 
@@ -25,57 +30,66 @@ class NotificationJobService: JobService() {
         Notifier.updateNotification(this)
 
         if (VideLibriApp.runningUpdates.isEmpty())
-            return false;
+            return false
 
-        Log.d("VIDELIBRI JOB", "onStartJob!! pending");
+        Log.d("VIDELIBRI JOB", "onStartJob!! pending")
 
-        instance = this;
-        pendingParams.add(params)
-        Handler().postDelayed({
-            //timeout. not supposed to happen
-            finishAll()
-        }, 1000*60*15)
+        val id = jobIndex(params)
+        if (id in pendingParams.indices) {
+            instance = this
+            pendingParams[id] = params
+            Handler().postDelayed({
+                //timeout. not supposed to happen
+                jobFinish(id)
+            }, 1000 * 60 * 15)
+        }
         return true
     }
 
     override fun onStopJob(params: JobParameters?): Boolean {
-        Log.d("VIDELIBRI JOB", "onStopJob!!");
-        pendingParams.remove(params)
+        Log.d("VIDELIBRI JOB", "onStopJob!!")
+        val id = jobIndex(params)
+        if (id in pendingParams.indices) pendingParams[id] = null
         return false
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        instance = null;
+        instance = null
+    }
+
+    private fun jobFinish(index: Int){
+        if (pendingParams[index] != null) {
+            instance?.jobFinished(pendingParams[index], false)
+            pendingParams[index] = null
+        }
     }
 
     companion object {
-        var instance: NotificationJobService? = null;
-        val pendingParams = mutableListOf<JobParameters?>()
+        var instance: NotificationJobService? = null
+        val pendingParams = arrayOfNulls<JobParameters?>(2)
 
         @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
         fun finishAll() {
-            Log.d("VIDELIBRI JOB", "finish all");
-            pendingParams.forEach { instance?.jobFinished(it, false) }
-            pendingParams.clear();
-            instance = null;
+            Log.d("VIDELIBRI JOB", "finish all")
+            instance?.jobFinish(0)
+            instance?.jobFinish(1)
+            instance = null
         }
     }
 }
 
-private const val JOB_ID_DAILY = 123
-private const val JOB_ID_AFTER_BOOT = 124
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 fun rescheduleDailyIfNecessaryAsJob(context: Context, afterDeviceBoot: Boolean){
-    val scheduler  = context.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler;
+    val scheduler  = context.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
     if (afterDeviceBoot) {
         val delay = NotificationScheduling.preferenceNotificationsBootDelayInMilliseconds(context)
         val b = JobInfo.Builder(JOB_ID_AFTER_BOOT, ComponentName(context, NotificationJobService::class.java))
-        b.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
-        b.setMinimumLatency(delay);
-        b.setOverrideDeadline(delay*2);
-        scheduler.schedule(b.build());
+        b.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+        b.setMinimumLatency(delay)
+        b.setOverrideDeadline(delay*2)
+        scheduler.schedule(b.build())
     }
 
     for (j in scheduler.allPendingJobs)
@@ -86,6 +100,6 @@ fun rescheduleDailyIfNecessaryAsJob(context: Context, afterDeviceBoot: Boolean){
     b.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
     b.setPeriodic(NotificationScheduling.DAILY_CHECK_PERIOD)
     b.setPersisted(true)
-    scheduler.schedule(b.build());
+    scheduler.schedule(b.build())
 }
 
