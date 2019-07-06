e07ce5e0597fc1c5d2b24c36ffc7fba4e3e85b28
benibela <benito@benibela.de>
2019-07-06 12:26:49 +0200

    try workaround for service starting failure report

--- android/android/src/de/benibela/videlibri/notifications/service/NotificationShowNow.java
+++ android/android/src/de/benibela/videlibri/notifications/service/NotificationShowNow.java
@@ -6,11 +6,24 @@ package de.benibela.videlibri.notifications.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.DeadObjectException;
 
 
 public class NotificationShowNow extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        context.startService(new Intent(context, NotificationService.class));
+        try {
+            context.startService(new Intent(context, NotificationService.class));
+        } catch (RuntimeException e) {
+            //retry. sometimes starting fails randomly? or use bindService? https://stackoverflow.com/questions/40994627/
+            e.printStackTrace();
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException ignore) {
+
+            }
+            context.startService(new Intent(context, NotificationService.class));
+        }
+
     }
 }
