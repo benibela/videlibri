b235fa728614d02c64d2f6ef2957497546eafe20
benibela <benito@benibela.de>
2019-08-16 17:31:44 +0200

    protect against accidental seekbar changes

--- android/android/src/de/benibela/videlibri/PreferenceSeekBar.kt
+++ android/android/src/de/benibela/videlibri/PreferenceSeekBar.kt
@@ -2,15 +2,17 @@ package de.benibela.videlibri
 
 
 import android.content.Context
-import android.content.res.TypedArray
 import android.graphics.Color
 import android.support.v7.preference.PreferenceViewHolder
 import android.support.v7.preference.SeekBarPreference
 import android.util.AttributeSet
 import android.widget.TextView
+import android.widget.SeekBar
 
 class PreferenceSeekBar @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = android.support.v7.preference.R.attr.seekBarPreferenceStyle, defStyleRes: Int = 0) : SeekBarPreference(context, attrs, defStyleAttr, defStyleRes) {
     private val dynamicSummary: String?
+    private var seekbarView: SeekBar? = null
+    private var editable: Boolean = false
 
     init {
 
@@ -32,12 +34,21 @@ class PreferenceSeekBar @JvmOverloads constructor(context: Context, attrs: Attri
         super.onBindViewHolder(view)
         //workaround  for https://issuetracker.google.com/issues/37130859
         (view.findViewById(android.R.id.title) as TextView?)?.setTextColor(Color.WHITE)
+        seekbarView = view.findViewById(android.support.v7.preference.R.id.seekbar) as SeekBar?
+        seekbarView?.isEnabled = editable
+    }
+
+    override fun onClick() {
+        super.onClick()
+        editable = true
+        seekbarView?.isEnabled = editable
+        showDynamicSummary()
     }
 
     private fun showDynamicSummary() {
-        if (dynamicSummary != null) {
-            //todo: plural https://stackoverflow.com/a/25648349
-            summary = String.format(dynamicSummary, value)
-        }
+        val s = dynamicSummary?.let { String.format(it, value) } ?: ""
+        //todo: plural https://stackoverflow.com/a/25648349
+        summary = if (editable) s
+                  else s + "   " +getString(R.string.lay_options_seekbar_tap_to_change);
     }
 }
