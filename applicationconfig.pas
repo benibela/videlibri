8521e631c17cefe4746c45f8027ac25d048569a2
benibela <benito@benibela.de>
2020-04-04 15:50:48 +0200

    version 2.166

--- applicationconfig.pas
+++ applicationconfig.pas
@@ -73,7 +73,7 @@ var programPath,userPath:string;
     nextLimitStr: string;
 
     appFullTitle:string='VideLibri';
-    versionNumber:integer=2164;
+    versionNumber:integer=2166;
     //=>versionNumber/1000
     newVersionInstalled: boolean=false;
 
