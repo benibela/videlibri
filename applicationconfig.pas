2e91253b72a8fdb551c67e0fa64acac36e8d5fb4
benibela <benito@benibela.de>
2024-02-18 21:29:30 +0100

    VideLibri 2.783 debug release

--- applicationconfig.pas
+++ applicationconfig.pas
@@ -72,7 +72,7 @@ var programPath,userPath:string;
     nextLimitStr: string;
 
     appFullTitle:string='VideLibri';
-    versionNumber:integer=2782;
+    versionNumber:integer=2783;
     versionBuildId: string = 'unknown';
     //=>versionNumber/1000
     newVersionInstalled: boolean=false;
