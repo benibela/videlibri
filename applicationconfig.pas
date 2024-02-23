f8d0f202c337013a7d78f6f39e975e302c3a71ba
benibela <benito@benibela.de>
2024-02-23 10:34:49 -0800

    2784 debug test version

--- applicationconfig.pas
+++ applicationconfig.pas
@@ -72,7 +72,7 @@ var programPath,userPath:string;
     nextLimitStr: string;
 
     appFullTitle:string='VideLibri';
-    versionNumber:integer=2783;
+    versionNumber:integer=2784;
     versionBuildId: string = 'unknown';
     //=>versionNumber/1000
     newVersionInstalled: boolean=false;
