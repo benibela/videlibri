6d51544c931b8eebdb812aade831ae413e7e6830
benibela <benito@benibela.de>
2025-11-19 19:55:39 +0100

    abort compilation if common interface is missing

--- manage.sh
+++ manage.sh
@@ -155,9 +155,12 @@ downloadTable)
     
     
   create-common-interface)
+    set -e
     xidel interface.pretty --module interface-generator-pretty-to-xml.xqm -e 'serialize(igp:make($raw), {"indent": true()})' | tee interface.xml
     xidel interface.xml --module interface-generator.xqm -e '"//This file has been generated automatically. Do not edit it, do not read it.", "//Refer to the interface.pretty file", ig:pascal-make(/)' | tee commoninterface.pas
+    if [ ! -s commoninterface.pas ]; then exit; fi
     xidel interface.xml --module interface-generator.xqm -e '"//This file has been generated automatically. Do not edit it, do not read it.", "//Refer to the interface.pretty file", ig:kotlin-make(/)' | tee android/android/src/de/benibela/videlibri/jni/CommonInterface.kt
+    if [ ! -s android/android/src/de/benibela/videlibri/jni/CommonInterface.kt ]; then exit; fi
     xidel interface.xml --module interface-generator.xqm -e 'ig:code-to-add-manually(/)' 
 #    xidel interface.xml --module interface-generator.xqm -e 'ig:pascal-kotlin-make-bridge(/)'
 
